3ac40d1159000b1e2009849ba4b444701d11169b
Brandon Anderson <brandanders85@gmail.com>
2021-01-31 03:22:59 -0700

    who can keep track anymore

--- GlobalGameJam2021/Assets/Scripts/Corkboard.cs
+++ GlobalGameJam2021/Assets/Scripts/Corkboard.cs
@@ -1,11 +1,36 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 [RequireComponent(typeof(Interactable))]
 public class Corkboard : MonoBehaviour, IInteraction
 {
+    [SerializeField]
+    private Transform corkboardMapTransform;
+
+    [SerializeField]
+    private TextMeshProUGUI buttonHelpText;
+
+    private bool _isPlayerInCorkboard = false;
+
+
+    private void Awake()
+    {
+        buttonHelpText.enabled = false;
+    }
     public void Interact()
     {
-        Debug.Log("Corkboard interacted with");
+        Player.Instance.transform.position = corkboardMapTransform.position;
+        _isPlayerInCorkboard = true;
+        buttonHelpText.enabled = true;
     }
-   
+
+    private void Update()
+    {
+        if (_isPlayerInCorkboard && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Player.Instance.transform.position = transform.position;
+            _isPlayerInCorkboard = false;
+            buttonHelpText.enabled = false;
+        }
+    }
 }
